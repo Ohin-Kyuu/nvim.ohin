871e45bb02feceeb2df97a6d8c8151bf48e94cca
Ohin-Kyuu <ohin.kyuu@gmail.com>
2026-08-25 22:05:06 +0800

    chore(phase7): remove legacy files with no runtime references

--- lua/configs/code/lsp/settings/ruff.lua
+++
@@ -1,9 +0,0 @@
-local base_on_attach = require("nvchad.configs.lspconfig").on_attach
-
-return {
-  on_attach = function(client, bufnr)
-    client.server_capabilities.hoverProvider = false
-
-    base_on_attach(client, bufnr)
-  end,
-}
